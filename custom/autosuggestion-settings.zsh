6a4de3caf7bf397d88be4cfc5241e5d40bb69c98
Exr0n <spotyie@gmail.com>
2020-01-19 20:10:52 -0800

    add custom/ so it works properly

---
+++ custom/autosuggestion-settings.zsh
@@ -0,0 +1,2 @@
+bindkey '^Z' autosuggest-accept
+ZSH_AUTOSUGGEST_STRATEGY=(completion match_prev_cmd history)
---
+++ custom/powerlevel9k_config.zsh
@@ -0,0 +1,3 @@
+# Set prompt
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs vcs)
