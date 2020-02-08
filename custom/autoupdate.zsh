a4482a096395c76525d8bb39061ad2002d76f116
Exr0n <spotyie@gmail.com>
2020-02-08 11:43:36 -0800

    autoupdate oh-my-zsh from source

--- custom/autoupdate.zsh
+++ custom/autoupdate.zsh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
-(git pull https://github.com/ohmyzsh/ohmyzsh.git \
+(\
+  [[ -n "$(git pull https://github.com/ohmyzsh/ohmyzsh.git | grep "Already up to date.")" ]]\
   && git add -A\
   && git commit -m "autoupdate oh-my-zsh from source"\
   && git push)&
