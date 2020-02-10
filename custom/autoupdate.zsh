37121e93142f428d27d2bf95beb9e9374412feec
Exr0n <spotyie@gmail.com>
2020-02-09 20:40:30 -0800

    give up on autoupdating ohmyzsh for now

--- custom/autoupdate.zsh
+++ custom/autoupdate.zsh
@@ -1,7 +1,10 @@
 #!/bin/zsh
 
+echo "$(git pull https://github.com/ohmyzsh/ohmyzsh.git | grep "Already up to date.")"
+
+exit
+
 (\
   [[ -n "$(git pull https://github.com/ohmyzsh/ohmyzsh.git | grep "Already up to date.")" ]]\
-  && git add -A\
-  && git commit -m "autoupdate oh-my-zsh from source"\
-  && git push)&
+  && echo "Pushing!"\
+)&
