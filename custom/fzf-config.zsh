016a6bbc2be6bcbbf31f77b2d1795730881b0a2b
Exr0n <spotyie@gmail.com>
2020-02-15 10:19:24 -0800

    add template jump keybind <Leader>g

--- custom/fzf-config.zsh
+++ custom/fzf-config.zsh
@@ -2,3 +2,6 @@
 
 # https://www.youtube.com/watch?v=1a5NiMhqAR0
 export FZF_DEFAULT_OPTS="--extended"
+
+export FZF_DEFAULT_COMMAND='fd -H --type f'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
